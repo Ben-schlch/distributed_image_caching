e5baf00e8a88f53037331a770eaacf320a74705b
Ben-schlch <inf21140@lehre.dhbw-suttgart.de>
2024-05-03 21:57:25 +0200

    readme

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # This is the python script that finally emulates the user which uses the client to download images from server cluster
-
-
 # goal is it to make optimal caching on client and server side
 # e.g. caching strategy, hit-rate, etc.
 
@@ -11,25 +9,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import asyncio
 from tqdm import tqdm
-import threading
 
-# import matplotlib
-# matplotlib.use('TkAgg')
 
 # set logging level to INFO
 import logging
-
 logging.basicConfig(level=logging.INFO)
+
 
 # Configuration
 random.seed(42)  # For reproducibility
 total_requests = 1000
 image_id_range = (1, 9999)
-# intitalize array for image id range with all zeros
 image_request_count = np.zeros(image_id_range[1] + 1)
-
 # backend_urls = [r"C:\Users\bensc\Projects\verteilte_system\images"]  # local debug
-backend_urls = [r"10.8.0.1:8002", "10.8.0.2:8002"]  # local debug
+backend_urls = [r"10.8.0.1:8002", "10.8.0.2:8002"]
+
 
 distributions = {
     "gaussian": {"mean": 5000, "std": 1200, "image_request_count": image_request_count.copy()},
@@ -81,12 +75,11 @@ async def main():
         "FIFO": Client(backend_urls, FIFO(), debug_local=False),
         "RR": Client(backend_urls, RandomReplacement(), debug_local=False)
     }
-    #tasks = [asyncio.create_task(client.start_listening_to_updates()) for client in clients.values()]
     # Simulate requests and plot results
     for distribution_name, params in distributions.items():
-        plt.clf()
-        plt.figure(figsize=(10, 6))
+        plt.figure(figsize=(16, 6))
 
+        plt.subplot(1, 2, 1)
         for name, client in clients.items():
             task = asyncio.create_task(client.start_listening_to_updates())
             client.strategy.cache = {}
@@ -109,24 +102,28 @@ async def main():
             client.listening_for_updates = False
             await task
 
+
         plt.xlabel('Request Count')
         plt.ylabel('Cache Hit Rate')
         plt.title(f'Cache Hit Rate over Time by Strategy with {distribution_name.capitalize()} Distribution')
         plt.legend()
-        plt.show()
+        # plt.show()
 
         # Plot image request count for each distribution
-        plt.clf()
-        plt.figure(figsize=(10, 6))
+        # plt.clf()#
+        plt.subplot(1, 2, 2)
+        # plt.figure(figsize=(10, 6))
         plt.plot(params["image_request_count"])
         plt.xlabel('Image ID')
         plt.ylabel('Request Count')
         plt.title(f'Image Request Count for {distribution_name.capitalize()} Distribution')
+
+        plt.tight_layout()
         plt.show()
 
     # boxplot of server response times across all distributions and client caching strategies
     plt.clf()
-    plt.figure(figsize=(10, 6))
+    plt.figure()
     plt.boxplot(server_response_times)
     plt.title('Server Response Times in ms across all tested distributions and client caching strategies')
 
